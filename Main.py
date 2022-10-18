154ad913b74de6c4d6a7d771ae1eb61b379d006d
Fatbonks <106663968+Fatbonks@users.noreply.github.com>
2022-10-19 09:50:20 +1300

    b

--- Main.py
+++ Main.py
@@ -1,1 +1,46 @@
-# main file will have nothing!
+def get_age():
+    while True:
+        try:
+            ans = int(input("What is your age: "))
+            age.insert(len(names), ans)
+            break
+        except ValueError:
+            print("please input a number")
+
+
+def get_name():
+    ans = input("What is your name: ")
+    names.insert(len(names), ans)
+
+
+def want_snack():
+    while True:
+        ans = input('Want Snack?: ').lower()
+        if ans == 'yes' or ans == 'y':
+          
+        elif ans == 'no' or ans == 'n':
+            print('Snacks order: NONE')
+            break
+
+        else:
+            print('please enter yes or no or y/n')
+
+
+valid_snacks = ['popcorn', 'm&m', 'candy', 'EOF']
+snacks_ordered = []
+names = []
+age = []
+max_tickets = 150
+tickets_sold = 0
+while True:
+    print()
+    try:
+        amount_of_people = int(input("How many people will be buying a ticket?: "))
+    except ValueError:
+        print("")
+    for i in range(amount_of_people):
+        get_name()
+        get_age()
+
+    print(names)
+    print(age)
